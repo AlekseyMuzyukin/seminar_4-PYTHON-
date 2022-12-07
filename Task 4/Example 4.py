a7abb1eef952d2268fc6d25b3d15cad5e13c41c6
Aleksey <J2k_pot@mail.ru>
2022-12-07 14:45:29 +0300

    add 5

--- Task 4/Example 4.py
+++ Task 4/Example 4.py
@@ -2,13 +2,13 @@ from random import randint
 max_val = 100
 k = int(input('Введите натуральную степень k:'))
 koeff = [randint(0, max_val) for i in range(k)]+[randint(1, max_val)]
-poly = '+'.join([f'{(j,"")[j==1]}x**{i}' for i,
+poly = '+'.join([f'{(j,"")[j==1]}*x**{i}' for i,
                 j in enumerate(koeff) if j][::-1])
-poly = poly.replace('x**1+', 'x+')
-poly = poly.replace('x**0', '')
+poly = poly.replace('*x**1+', '*x+')
+poly = poly.replace('*x**0', '')
 poly += ('', '1')[poly[-1] == '+']
 poly = (poly, poly[:-2])[poly[-2:] == '**1']
 
-data = open('file_2.txt','a')
-data.writelines(f'{poly}\n')
+data = open('file.txt','w')
+data.writelines(f'{poly} ')
 data.closed
